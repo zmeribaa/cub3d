13e91c776801d62143c4c81bcbfe973cdc5a704b
Ziyad Meribaa <zmeribaa@e2r2p3.1337.ma>
2022-04-19 20:30:01 +0000

    First commit

---
+++ Includes/cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zmeribaa <zmeribaa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 21:20:44 by zmeribaa          #+#    #+#             */
+/*   Updated: 2022/04/18 21:21:42 by zmeribaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include <mlx.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#endif
